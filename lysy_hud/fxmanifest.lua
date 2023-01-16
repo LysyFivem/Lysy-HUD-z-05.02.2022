4ab35ad086a9541779fe14216a435c871001a80d
LysyFivem <105931670+LysyFivem@users.noreply.github.com>
2023-01-16 13:57:36 +0100

    Add files via upload

---
+++ lysy_hud/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'bodacious'
+game 'gta5'
+author 'Łysy#0216'
+lua54 'yes'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'client.lua'
+}
+
+files {
+	'html/index.html',
+	'html/style.css',
+	'html/index.js',
+	'html/items/**.png',
+}
+
+export 'InVehicle'
+export 'UpdateVoice'
